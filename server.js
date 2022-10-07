b7f05feabf93ce9250d9c6d2a6d35ad1a9e602dc
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-10-07 20:34:15 +0530

    added routes

---
+++ server.js
@@ -0,0 +1,15 @@
+const app = require('./app')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config({path:'./config.env'})
+const port = process.env.PORT||5000;
+const db = process.env.DB.replace('<PASSWORD>',process.env.DB_PASSWORD)
+
+// mongoose.connect(db).then(()=>{
+//     console.log('Mongodb connection was successful')
+// })
+
+app.listen(port, ()=>{
+    console.log(`server is listening at port: ${port}`)
+})
